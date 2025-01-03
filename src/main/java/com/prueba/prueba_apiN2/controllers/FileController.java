a753c6d0025f60ac5d93b6b7ecd8dddbbea16b50
josefperea <jose.perea2000@hotmail.com>
2025-01-03 01:54:29 -0500

    Mejora para crear carpetas uploads/productos si no existieran.

--- src/main/java/com/prueba/prueba_apiN2/controllers/FileController.java
+++ src/main/java/com/prueba/prueba_apiN2/controllers/FileController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -21,8 +22,18 @@ public class FileController {
     public ResponseEntity<Resource> getFile(@PathVariable String filename) throws MalformedURLException {
         //La ruta la debe tomar segun el directorio donde se aloje el proyecto, para que se lea desde cualquier pc sin problema.
         String basePath = System.getProperty("user.dir") + "/uploads/productos";
+        File directorio = new File(basePath);
         Path filePath = Paths.get(basePath).resolve(filename);
         Resource file = new UrlResource(filePath.toUri());
+
+        if (!directorio.exists()) {
+            //Si no existen las carpetas, se crean para que pueda guardar y leer las imagenes.
+            if (directorio.mkdirs()) {
+                System.out.println("Las carpetas se han creado: " + basePath);
+            } else {
+                System.out.println("No se pudieron crear las carpetas: " + basePath);
+            }
+        }
 
         if (file.exists() && file.isReadable()) {
             return ResponseEntity.ok()
